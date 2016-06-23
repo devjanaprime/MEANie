57cb04c3c7b88dcac01ce5afdbaa04513edeec50
devjanaprime <dev@primeacademy.io>
2016-06-23 14:46:20 -0500

    no comments or spacing

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,37 +1,28 @@
 var myApp=angular.module( 'myApp', [] );
-// create a controller
 myApp.controller( 'whereMyPeeps', [ '$scope', '$http', function( $scope, $http ){
-  $scope.addRecord = function(){
-    event.preventDefault();
-    // get the user input and store in an object
-    var objectToSend ={
-      name: $scope.nameIn,
-      location: $scope.locationIn
-    };
-    // make a call to server with object to be stored in DB
-    $http({
-      method: 'POST',
-      url: '/testPost',
-      data: objectToSend
-    });
-    // clear inputs
-    $scope.nameIn ='';
-    $scope.locationIn='';
-  }; // end addRecord
-  $scope.getRecords = function(){
-    // read button clicked
-    // retrieve records via http call (GET)
-    $http({
-      method: 'GET',
-      url: '/getRecords',
-    }).then( function( response ){
-      // like an ajax success
-      // we have been sent back "response"
-      // .data is the data in the reponse
-      $scope.allTheRecords = response; // .data;
-      console.log( $scope.allTheRecords );
-    }), function myError( response ){
-      console.log( response.statusText );
-    };
-  }; // end getRecords
-}]); // end whereMyPeeps controller
+$scope.addRecord = function(){
+event.preventDefault();
+var objectToSend ={
+name: $scope.nameIn,
+location: $scope.locationIn
+};
+$http({
+method: 'POST',
+url: '/testPost',
+data: objectToSend
+});
+$scope.nameIn ='';
+$scope.locationIn='';
+};
+$scope.getRecords = function(){
+$http({
+method: 'GET',
+url: '/getRecords',
+}).then( function( response ){
+$scope.allTheRecords = response; // .data;
+console.log( $scope.allTheRecords );
+}), function myError( response ){
+console.log( response.statusText );
+};
+};
+}]);
