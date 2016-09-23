3e43c0f1a01daabf374278ba18ed384dd8850742
devjanaprime <dev@primeacademy.io>
2016-09-23 13:59:49 -0500

    cleaned up some spacing

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,28 +1,28 @@
 var myApp=angular.module( 'myApp', [] );
 myApp.controller( 'whereMyPeeps', [ '$scope', '$http', function( $scope, $http ){
-$scope.addRecord = function(){
-event.preventDefault();
-var objectToSend ={
-name: $scope.nameIn,
-location: $scope.locationIn
-};
-$http({
-method: 'POST',
-url: '/testPost',
-data: objectToSend
-});
-$scope.nameIn ='';
-$scope.locationIn='';
-};
-$scope.getRecords = function(){
-$http({
-method: 'GET',
-url: '/getRecords',
-}).then( function( response ){
-$scope.allTheRecords = response; // .data;
-console.log( $scope.allTheRecords );
-}), function myError( response ){
-console.log( response.statusText );
-};
-};
+  $scope.addRecord = function(){
+  event.preventDefault();
+  var objectToSend ={
+    name: $scope.nameIn,
+    location: $scope.locationIn
+  };
+  $http({
+    method: 'POST',
+    url: '/testPost',
+    data: objectToSend
+  });
+    $scope.nameIn ='';
+    $scope.locationIn='';
+  };
+  $scope.getRecords = function(){
+    $http({
+      method: 'GET',
+      url: '/getRecords',
+    }).then( function( response ){
+      $scope.allTheRecords = response; // .data;
+      console.log( $scope.allTheRecords );
+    }), function myError( response ){
+      console.log( response.statusText );
+    };
+  };
 }]);
