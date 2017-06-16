4fd1eaa36c6d25c6ae45a9f891a3abc51e42f6b7
devjanaprime <dev@primeacademy.io>
2017-06-15 20:19:49 -0500

    working version 4

---
+++ public/scripts/services/peepsService.js
@@ -0,0 +1,21 @@
+myApp.service( 'PeepsService', function( $http ){
+  var sv = this;
+  sv.addPeep = function( newPeep ){
+    console.log( 'in addRecord:', newPeep );
+    return $http( {
+      method: 'POST',
+      url: '/peeps',
+      data: newPeep
+    });
+  };
+
+  sv.getPeeps = function(){
+    console.log( 'in getRecords' );
+    return $http({
+      method: 'GET',
+      url: '/peeps',
+    }).then( function( response ){
+      sv.allMyPeeps = response;
+    });
+  };
+});
--- server/app.js
+++ server/app.js
@@ -2,37 +2,13 @@ var express = require( 'express' );
 var app=express();
 var path = require( 'path' );
 var bodyParser = require( 'body-parser' );
+var index = require( '../modules/routes/index.js' );
+var peeps = require( '../modules/routes/peeps.js' );
+app.use( '/', index );
+app.use( '/peeps', peeps );
 app.use( bodyParser.json() );
-var mongoose = require( 'mongoose' );
-// 27017 is default mongo port
-mongoose.connect( 'localhost:/27017/meanie' );
-var ourSchema = new  mongoose.Schema({
-name: String,
-location: String
-});
-var ourModel = mongoose.model( 'ourModel', ourSchema );
-app.get( '/', function( req, res ){
-res.sendFile( path.resolve( 'public/index.html' ) );
-});
-app.get( '/getRecords', function( req, res ){
-// get and send back all the things
-ourModel.find().then( function( data ){
-res.send( data );
-});
-});
+app.use( express.static( 'public' ) );
+
 app.listen( 8080, 'localhost', function( req, res ){
 console.log( 'listening on 8080' );
 });
-app.post( '/testPost', function( req, res ){
-console.log( 'req.body.name: ' + req.body.name );
-// retrieved the req.body
-// putting it into an object to be saved in the db
-var recordToAdd={
-name:req.body.name,
-location:req.body.location
-}
-// create new record
-var newRecord=ourModel( recordToAdd );
-newRecord.save();
-});
-app.use( express.static( 'public' ) );
